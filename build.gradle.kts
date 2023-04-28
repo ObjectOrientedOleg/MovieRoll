c6fdffa9b648aea75198101020e28debdff5d30b
ObjectOrientedOleg <osiboglov@gmail.com>
2023-04-28 19:20:28 -0400

    added secrets plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,4 +9,5 @@ plugins {
     alias(libs.plugins.kotlin.kapt) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.secrets) apply false
 }
